08a8db23fe1f4873e34fc2fb96cdd8fcb4388762
JorenSynaeveTM <joren.synaeve@thomasmore.be>
2023-11-24 11:45:45 +0100

    Bugfix data collection from Canvas

--- GradebookDashboard/index.py
+++ GradebookDashboard/index.py
@@ -3,74 +3,72 @@ import dash_bootstrap_components as dbc
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.graph_objs as go
-
-# Read the data from 'data.csv' file but skip the second and third row
-DF = pd.read_csv('data.csv', skiprows=[1, 2])
-
 from src import data_collector
 
 data_collector = data_collector.DataCollector()
 # Get all canvas data
-data_collector.get_assignments()
-data_collector.get_sections()
-
-
-def get_section_dropdown():
-    sections = DF['Section'].unique()
-    labels = []
-    values = []
-    for section in sections:
-        # If the section is not a string, skip it
-        if type(section) != str:
-            continue
-        labels.append(section)
-        values.append(section)
-    print(labels)
-    print(values)
-    return dcc.Dropdown(options=labels, value=values)
-
-
-def get_assignment_dropdown():
-    assignments = DF['Assignment'].unique()
-    labels = []
-    values = []
-    for assignment in assignments:
-        # If the assignment is not a string, skip it
-        if type(assignment) != str:
-            continue
-        labels.append(assignment)
-        values.append(assignment)
-    return dcc.Dropdown(options=labels, value=values)
-
-
-def get_amount_of_students():
-    return len(DF.index)
-
-
-# Initialize the app
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-fig = go.Figure(data=[go.Scatter(x=['Joren', 'Siebe', 'Jente'], y=[4, 1, 2])])
-
-# App layout
-app.layout = html.Div(className='container', children=[
-    html.Div(className='row', children=[
-        html.Div(className='col-4', children=[
-            get_section_dropdown()
-        ]),
-        html.Div(className='col-8', children=f'There is a total of {get_amount_of_students()} students'),
-    ]),
-    html.Div(className='row', children=[
-        html.Div(className='col-12', children=[
-            dash_table.DataTable(DF.to_dict('records'), [{"name": i, "id": i} for i in DF.columns],
-                                 filter_action='native',
-                                 filter_options={"placeholder_text": "Filter column..."},
-                                 page_size=200)
-        ])
-    ]),
-    dcc.Graph(figure=fig)
-])
-
-# Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+data_collector.collect_data()
+#
+# # Read the data from 'data.csv' file but skip the second and third row
+# DF = pd.read_csv('data.csv', skiprows=[1, 2])
+#
+#
+# def get_section_dropdown():
+#     sections = DF['Section'].unique()
+#     labels = []
+#     values = []
+#     for section in sections:
+#         # If the section is not a string, skip it
+#         if type(section) != str:
+#             continue
+#         labels.append(section)
+#         values.append(section)
+#     print(labels)
+#     print(values)
+#     return dcc.Dropdown(options=labels, value=values)
+#
+#
+# def get_assignment_dropdown():
+#     assignments = DF['Assignment'].unique()
+#     labels = []
+#     values = []
+#     for assignment in assignments:
+#         # If the assignment is not a string, skip it
+#         if type(assignment) != str:
+#             continue
+#         labels.append(assignment)
+#         values.append(assignment)
+#     return dcc.Dropdown(options=labels, value=values)
+#
+#
+# def get_amount_of_students():
+#     return len(DF.index)
+#
+#
+# # Initialize the app
+# app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+#
+# fig = go.Figure(data=[go.Scatter(x=['Joren', 'Siebe', 'Jente'], y=[4, 1, 2])])
+#
+# # App layout
+# app.layout = html.Div(className='container', children=[
+#     html.Div(className='row', children=[
+#         html.Div(className='col-4', children=[
+#             get_section_dropdown()
+#         ]),
+#         html.Div(className='col-8', children=f'There is a total of {get_amount_of_students()} students'),
+#     ]),
+#     html.Div(className='row', children=[
+#         html.Div(className='col-12', children=[
+#             dash_table.DataTable(DF.to_dict('records'), [{"name": i, "id": i} for i in DF.columns],
+#                                  filter_action='native',
+#                                  filter_options={"placeholder_text": "Filter column..."},
+#                                  page_size=200)
+#         ])
+#     ]),
+#     dcc.Graph(figure=fig)
+# ])
+#
+# # Run the app
+# if __name__ == '__main__':
+#     app.run(debug=True)
--- GradebookDashboard/src/data_collector.py
+++ GradebookDashboard/src/data_collector.py
@@ -5,6 +5,47 @@ import xlsxwriter
 OUTPUT_FILE = 'canvas_data.xlsx'
 
 
+def create_excel_file(canvas_assignment_groups, sections_with_students):
+    workbook = xlsxwriter.Workbook(OUTPUT_FILE)
+    worksheet = workbook.add_worksheet("Assignment Groups")
+    worksheet.write_row(0, 0, ["Assignment group name"])
+    for index, assignment_group in enumerate(canvas_assignment_groups):
+        print(f"Writing assignment group {assignment_group.name} to excel file")
+        worksheet.write_row(index + 1, 0, [assignment_group.name])
+    # Create a worksheet for the assignments
+    worksheet = workbook.add_worksheet("Assignments")
+    worksheet.write_row(0, 0, ["Assignment name", "Assignment group name"])
+    for assignment_group in canvas_assignment_groups:
+        for index, assignment in enumerate(assignment_group.assignments):
+            print(f"Writing assignment {assignment['name']} to excel file")
+            worksheet.write_row(index + 1, 0, [assignment['name'], assignment_group.name])
+    # Create a worksheet for the sections
+    worksheet = workbook.add_worksheet("Sections")
+    worksheet.write_row(0, 0, ["Section name"])
+    for index, section in enumerate(sections_with_students):
+        # If the name contains a '|', skip it
+        if '|' in section.name:
+            continue
+        worksheet.write_row(index + 1, 0, [section.name])
+    # Create a worksheet for the students
+    worksheet = workbook.add_worksheet("Students")
+    worksheet.write_row(0, 0, ["Student name", "Section name"])
+    student_counter = 0
+    for section in sections_with_students:
+        print("Writing section " + section.name + " to excel file")
+        # If the name contains a '|', skip it
+        if '|' in section.name:
+            continue
+        # If the section has no students, skip it
+        if not section.students:
+            continue
+        for index, student in enumerate(section.students):
+            print(f"Writing student {student['name']} to excel file")
+            worksheet.write_row(student_counter + 1, 0, [student['name'], section.name])
+            student_counter += 1
+    workbook.close()
+
+
 class DataCollector:
     course_obj = None
 
@@ -21,45 +62,17 @@ class DataCollector:
         canvas_obj = Canvas("https://thomasmore.instructure.com/", api_key)
         self.course_obj = canvas_obj.get_course(course_id)
 
-    def get_assignments(self):
+    def collect_data(self):
+        canvas_assignment_groups = self.get_assignments_with_groups()
+        sections_with_students = self.get_sections()
+        create_excel_file(canvas_assignment_groups, sections_with_students)
+
+    def get_assignments_with_groups(self):
         # Get all assignment groups in the course with their assignments
         canvas_assignment_groups = self.course_obj.get_assignment_groups(include=['assignments'])
-        # Write the assignment groups to the output file
-        workbook = xlsxwriter.Workbook(OUTPUT_FILE)
-        worksheet = workbook.add_worksheet("Assignment Groups")
-        worksheet.write_row(0, 0, ["Assignment group name"])
-        for index, assignment_group in enumerate(canvas_assignment_groups):
-            worksheet.write_row(index + 1, 0, [assignment_group.name])
-        # Create a worksheet for the assignments
-        worksheet = workbook.add_worksheet("Assignments")
-        worksheet.write_row(0, 0, ["Assignment name", "Assignment group name"])
-        for index, assignment_group in enumerate(canvas_assignment_groups):
-            for assignment in assignment_group.assignments:
-                worksheet.write_row(index + 1, 0, [assignment['name'], assignment_group.name])
-        workbook.close()
+        return canvas_assignment_groups
 
     def get_sections(self):
         # Get all sections in the course with their students
         canvas_sections = self.course_obj.get_sections(include=['students'])
-        # Write the sections to the output file
-        workbook = xlsxwriter.Workbook(OUTPUT_FILE)
-        worksheet = workbook.add_worksheet("Sections")
-        worksheet.write_row(0, 0, ["Section name"])
-        for index, section in enumerate(canvas_sections):
-            # If the name contains a '|', skip it
-            if '|' in section.name:
-                continue
-            worksheet.write_row(index + 1, 0, [section.name])
-        # Create a worksheet for the students
-        worksheet = workbook.add_worksheet("Students")
-        worksheet.write_row(0, 0, ["Student name", "Section name"])
-        for index, section in enumerate(canvas_sections):
-            # If the name contains a '|', skip it
-            if '|' in section.name:
-                continue
-            # If the section has no students, skip it
-            if not section.students:
-                continue
-            for student in section.students:
-                worksheet.write_row(index + 1, 0, [student['name'], section.name])
-        workbook.close()
+        return canvas_sections
